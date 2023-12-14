b4ad2fb38143e8bc3643a059d65e954d2ede4947
AlexAksioma <alexmedqwerty@gmail.com>
2023-12-14 19:53:13 +0200

    added login, create new board

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -4,10 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class HelperBase {
     WebDriver driver;
     public HelperBase(WebDriver driver) {
         this.driver = driver;
+    }
+
+    private WebElement findElementBase(By locator){
+        System.out.println(locator);
+        return driver.findElement(locator);
+    }
+
+    private List<WebElement> findElementsBase(By locator){
+        System.out.println(locator);
+        return driver.findElements(locator);
     }
 
     public void pause(int time){
@@ -19,7 +31,22 @@ public class HelperBase {
     }
 
     public void clickBase(By locator){
-        WebElement element = driver.findElement(locator);
+        WebElement element = findElementBase(locator);
         element.click();
+    }
+    public void typeBase(By locator, String text){
+        WebElement element = findElementBase(locator);
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public boolean isElementPresent(By locator){       //return list.size() > 0
+        return !findElementsBase(locator).isEmpty();
+    }
+
+    public boolean isTextInElementEquals(By locator, String text){
+        WebElement element = findElementBase(locator);
+        return element.getText().equals(text);
     }
 }
